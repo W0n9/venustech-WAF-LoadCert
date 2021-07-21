a69220467bec54cea037ebbfc901f2571151f68a
w0n9 <38560218+W0n9@users.noreply.github.com>
2021-07-21 11:18:04 +0800

    inital

---
+++ main.py
@@ -0,0 +1,59 @@
+# %%
+import hashlib
+import json
+import time
+
+import requests
+
+timestamp = int(round(time.time() * 1000))
+
+
+def getStrAsMD5(parmStr):
+    #1、参数必须是utf8
+    #2、python3所有字符都是unicode形式，已经不存在unicode关键字
+    #3、python3 str 实质上就是unicode
+    if isinstance(parmStr, str):
+        # 如果是unicode先转utf-8
+        parmStr = parmStr.encode("utf-8")
+    m = hashlib.new('md5')
+    m.update(parmStr)
+    return m.hexdigest()
+
+
+def readConfig(filename):
+    with open(filename, "r") as f:
+        config = json.load(f)
+        return config
+
+
+config = readConfig("config.json")
+ts = str(timestamp)
+api_uri = "/v2/certInfo"
+user = config["user"]
+password = config["password"]
+
+sign = user + ':' + getStrAsMD5(ts + api_uri + user + getStrAsMD5(password))
+
+baseURL = config["baseURL"]
+url = baseURL + api_uri
+
+payload = {
+    "action": "show",
+    "ts": ts,
+    "sign": sign,
+}
+
+data = {
+    "cert_file_name": "wildcard.crt",
+    "key_file_name": "wildcard.key",
+    "type": 3,
+    "password": "",
+    "cert_file_content": "",
+    "key_file_content": "",
+    "upload_location": 0
+}
+
+r = requests.get(url, params=payload, allow_redirects=False)
+print(r.json())
+
+# %%
